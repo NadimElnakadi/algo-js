69d025f6a92baaaf7b65e5c73532f299c2ce6f67
Nadim El Nakadi <nadim.elnakadi@gmail.com>
2022-11-24 16:26:14 +0100

    nadim

---
+++ 3.0.js
@@ -0,0 +1,5 @@
+let Keller5=  ["Arno Volts" , "Aurelien Mariaule" ,"Aurore Lemaire" ,"Benjamin Porsont" ,"Céline Verreydt" ,"Corentin Miserque" ,"Dominique Coppée" ,"Edouard     de Romrée de Vichenet" ,"Hugo Goorickx ","Jofrey Houyoux" ,"Jonathan Manes" ,"jonathan bajoux" ,"Laura Wilhelmi" ,"Lysie Soyez" ,"Marnie Benalia" ,"Mathilde     Cornelis" ,"Milo Bonnet" ,"Nadim El Nakadi" ,"Nathalie Vanden Abeele" ,"Nathalie Goffette" ,"Nour Everaert" ,"Pierre Mauriello" ,"Quentin Bource ","Virginie Dourson"];
+
+for (let elem of Keller5){
+    console.log(elem);
+}
