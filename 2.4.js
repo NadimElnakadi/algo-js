642a773f445cb22fa8f6f9907d8e5811b8e404f0
Nadim El Nakadi <nadim.elnakadi@gmail.com>
2022-11-24 14:09:37 +0100

    nadim

---
+++ 2.4.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 100; i += 1) {
+    if (i%2 == 0) {
+        console.log(i/2)
+    }
+    else if (i%2 ==1){
+        console.log(i*3)
+    }
+
+  }
---
+++ 2.5.js
@@ -0,0 +1,13 @@
+
+
+let i = prompt("What is your favourite number ?");
+
+while (i!==42){
+    let a = prompt("Are you sure ?");
+    if (a==42){
+        break;
+    }
+}
+  
+  
+
