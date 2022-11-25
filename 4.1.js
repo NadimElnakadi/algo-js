1286d4691e61915b247b7eee7aed09cdbd4dc339
Nadim El Nakadi <nadim.elnakadi@gmail.com>
2022-11-25 16:07:18 +0100

    nadim

--- 4.1.js
+++ 4.1.js
@@ -1,5 +1,7 @@
+let l = prompt("Longueur");
+let n = prompt("largeur")
 function calcSurface(length, width){
     return  width* length
 }
-console.log (calcSurface(8,9))
+console.log (calcSurface(l,n))
 // Cette fonction permet de multiplier la longueur et la largeur du rectangle afin de calculer sa surface. 
