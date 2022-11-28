29816e0f31354d956a5dd0c326fe4e45cdf31c7c
Nadim El Nakadi <nadim.elnakadi@gmail.com>
2022-11-28 09:48:32 +0100

    nadim

---
+++ 4.6.js
@@ -0,0 +1,19 @@
+
+let n = prompt("enter a number");
+
+function factorialize(num) {
+   
+    if (num < 0) 
+          return -1;
+      
+
+    else if (num == 0) 
+        return 1;
+      
+      else {
+          return (num * factorialize(num - 1));
+          
+      }
+  }
+  console.log(factorialize(n));
+  
